ef416b79ec7ab259ad2e849d0aefcfc5c492a809
Chaz Chandler <clc31@inbox.com>
2019-12-01 23:26:08 -0500

    WIP: irsa

--- irsa.tf
+++ irsa.tf
@@ -13,3 +13,27 @@ resource "aws_iam_openid_connect_provider" "oidc_provider" {
   thumbprint_list = [var.eks_oidc_root_ca_thumbprint]
   url             = flatten(concat(aws_eks_cluster.this[*].identity[*].oidc.0.issuer, [""]))[0]
 }
+
+data "aws_iam_policy_document" "irsa_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.oidc_provider.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.oidc_provider.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "irsa_example" {
+  assume_role_policy = data.aws_iam_policy_document.irsa_assume_role_policy.json
+  name               = "irsa-example"
+  path               = var.iam_path
+}
